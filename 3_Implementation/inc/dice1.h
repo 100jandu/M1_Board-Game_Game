c529f542e28e1c683547695c15a98d3d56133add
100jandu <rsaujandu25@gmail.com>
2022-03-03 19:07:25 +0530

    make file updated

--- 3_Implementation/inc/dice1.h
+++ 3_Implementation/inc/dice1.h
@@ -1,9 +1,13 @@
 #ifndef _DICE_H_
 #define _DICE_H_
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
  * @brief Function to find the output of a dice roll
  * @return int result of dice_output operation
  * 
  */
 int dice_output();
- #endif/*_DICE_H_*/
+#endif/*_DICE_H_*/
