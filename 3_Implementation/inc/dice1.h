aeccad8e5b33dc5e5703aefdd1161cb7582396c8
100jandu <rsaujandu25@gmail.com>
2022-03-04 12:22:51 +0530

    Doxy file updated

--- 3_Implementation/inc/dice1.h
+++ 3_Implementation/inc/dice1.h
@@ -1,6 +1,15 @@
 #ifndef _DICE_H_
 #define _DICE_H_
-
+/**
+ * @file dice1.h
+ * @author Saujandu Roy 
+ * @brief This project is a board game of Snake and ladder
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
